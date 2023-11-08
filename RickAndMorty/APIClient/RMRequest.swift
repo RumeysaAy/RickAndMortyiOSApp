f2061c54f100c1b76d393fa1169daad8b051b9e0
RumeysaAy <rumeeysaay@gmail.com>
2023-11-08 20:55:31 +0300

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Rumeysa Ay on 8.11.2023.
+//
+
+import Foundation
+
+/// Object that represents a singlet API call
+final class RMRequest {
+    
+    
+}
